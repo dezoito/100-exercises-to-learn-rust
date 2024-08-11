de0a2c4af69d929bd202afe515f504f0c2614c26
dezoito <an.dezoito@gmail.com>
2024-08-11 19:08:32 -0300

    Channels exercise and comments

--- exercises/07_threads/05_channels/src/lib.rs
+++ exercises/07_threads/05_channels/src/lib.rs
@@ -1,6 +1,7 @@
 use std::sync::mpsc::{Receiver, Sender};
 
 use data::TicketDraft;
+use store::TicketStore;
 
 pub mod data;
 pub mod store;
@@ -12,6 +13,21 @@ pub enum Command {
 // Start the system by spawning the server the thread.
 // It returns a `Sender` instance which can then be used
 // by one or more clients to interact with the server.
+
+/*
+* You really have to look at tests/insert.rs to solve this one
+* as there the lesson doesn't help at all
+*
+* 1- Here, we create an enum "Command", that defines an action
+*    where the payload is a draft.
+*    launch() creates the channel, sender and receiver
+* 2- On insert.rs, we invoke the sender and use it to send the
+*    "Insert" command for a new draft
+* 3- Here, on server, we loop while we receive commands,
+*    and if one matches the "Insert" enum, we add the draft to
+*    the ticket store
+* https://chatgpt.com/share/5fb2212a-e840-4757-ac37-dd5b2d211af5
+*/
 pub fn launch() -> Sender<Command> {
     let (sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
@@ -23,7 +39,12 @@ pub fn launch() -> Sender<Command> {
 //  the channel, then execute it, then start waiting
 //  for the next command.
 pub fn server(receiver: Receiver<Command>) {
-    loop {
-        let sender = launch();
+    let mut store = TicketStore::new();
+    while let Ok(command) = receiver.recv() {
+        match command {
+            Command::Insert(ticket_draft) => {
+                store.add_ticket(ticket_draft);
+            }
+        }
     }
 }
