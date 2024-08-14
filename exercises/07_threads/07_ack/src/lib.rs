c2ce5006e7bd000e04766a08eccacde95ce4e932
dezoito <an.dezoito@gmail.com>
2024-08-14 15:13:14 -0300

    Two way communication exercise (ack)

--- exercises/07_threads/07_ack/src/lib.rs
+++ exercises/07_threads/07_ack/src/lib.rs
@@ -1,13 +1,32 @@
+use data::{Ticket, TicketDraft};
 use std::sync::mpsc::{Receiver, Sender};
+use store::TicketId;
+
 use crate::store::TicketStore;
 
 pub mod data;
 pub mod store;
 
 // Refer to the tests to understand the expected schema.
+
+/*
+* The Insert command expects a draft and a sender for the id
+* hence why its type is Sender<TicketId>
+*
+* The Get command expects an id (of type TicketId), and
+* a sender for the Ticket... since tickets may or may not exist,
+* we have to wrap them in an Option<>
+*
+*/
 pub enum Command {
-    Insert { todo!() },
-    Get { todo!() }
+    Insert {
+        draft: TicketDraft,
+        response_sender: Sender<TicketId>,
+    },
+    Get {
+        id: TicketId,
+        response_sender: Sender<Option<Ticket>>,
+    },
 }
 
 pub fn launch() -> Sender<Command> {
@@ -21,19 +40,31 @@ pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
-            Ok(Command::Insert {}) => {
-                todo!()
-            }
-            Ok(Command::Get {
-                todo!()
+            // * add the correct params for the enum
+            Ok(Command::Insert {
+                draft,
+                response_sender,
             }) => {
-                todo!()
+                // * get the ticket id and send it though the channel
+                // * let _ is required by the compiler
+                let id = store.add_ticket(draft);
+                let _ = response_sender.send(id);
+            }
+            // * add the correct params for the enum
+            Ok(Command::Get {
+                id,
+                response_sender,
+            }) => {
+                // * must return a ticket
+                // * let _ is required by the compiler
+                let ticket = store.get(id);
+                let _ = response_sender.send(ticket.cloned());
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
                 // and shut down the server.
-                break
-            },
+                break;
+            }
         }
     }
 }
