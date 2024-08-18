84fbf1bd338316b2f1eb1b509fdae773c6eb08c7
dezoito <an.dezoito@gmail.com>
2024-08-18 12:19:38 -0300

    Bounded channel exercise and comments

--- exercises/07_threads/09_bounded/src/lib.rs
+++ exercises/07_threads/09_bounded/src/lib.rs
@@ -1,40 +1,72 @@
 // TODO: Convert the implementation to use bounded channels.
 use crate::data::{Ticket, TicketDraft};
 use crate::store::{TicketId, TicketStore};
-use std::sync::mpsc::{Receiver, Sender};
 
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 pub mod data;
 pub mod store;
 
+// * The error type has to be manually defined
+// * since we are using try_send
+#[derive(Debug, thiserror::Error)]
+#[error("The store is overloaded")]
+pub struct OverloadedError;
+
+// * Notice we have to use SyncSender
 #[derive(Clone)]
 pub struct TicketStoreClient {
-    sender: todo!(),
+    sender: SyncSender<Command>,
 }
 
 impl TicketStoreClient {
-    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, todo!()> {
-        todo!()
+// * we now have to map errors and return a Result (try_send can err)
+impl TicketStoreClient {
+    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, OverloadedError> {
+        // * define sender and receiver for the SYNC channel (limited to 1 msg)
+        let (response_sender, response_receiver) = std::sync::mpsc::sync_channel(1);
+
+        // * this defines the sender for a TicketStoreClient
+        // * it is the client invoked on the tests
+        self.sender
+            .try_send(Command::Insert {
+                draft,
+                response_channel: response_sender,
+            })
+            .map_err(|_| OverloadedError)?;
+        // * we have to Ok, since we are returning a result
+        Ok(response_receiver.recv().unwrap())
     }
 
-    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, OverloadedError> {
+        let (response_sender, response_receiver) = std::sync::mpsc::sync_channel(1);
+        self.sender
+            .try_send(Command::Get {
+                id,
+                response_channel: response_sender,
+            })
+            .map_err(|_| OverloadedError)?;
+        Ok(response_receiver.recv().unwrap())
     }
 }
 
+// * capacity to define # of messages
 pub fn launch(capacity: usize) -> TicketStoreClient {
-    todo!();
+    let (sender, receiver) = sync_channel(capacity);
+
     std::thread::spawn(move || server(receiver));
-    todo!()
+    // * Look at the return type!
+    TicketStoreClient { sender }
 }
 
+// * Commands now use SyncSender
 enum Command {
     Insert {
         draft: TicketDraft,
-        response_channel: todo!(),
+        response_channel: SyncSender<TicketId>,
     },
     Get {
         id: TicketId,
-        response_channel: todo!(),
+        response_channel: SyncSender<Option<Ticket>>,
     },
 }
 
@@ -47,14 +79,14 @@ pub fn server(receiver: Receiver<Command>) {
                 response_channel,
             }) => {
                 let id = store.add_ticket(draft);
-                todo!()
+                let _ = response_channel.send(id);
             }
             Ok(Command::Get {
                 id,
                 response_channel,
             }) => {
                 let ticket = store.get(id);
-                todo!()
+                let _ = response_channel.send(ticket.cloned());
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
