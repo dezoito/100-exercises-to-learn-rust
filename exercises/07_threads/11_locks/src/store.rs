82a88ad9e21f7c6a334c2ee35f3bf5e061bee100
dezoito <an.dezoito@gmail.com>
2024-09-02 16:21:28 -0300

    Lock exercise, using Arc<Mutex<T>>

--- exercises/07_threads/11_locks/src/store.rs
+++ exercises/07_threads/11_locks/src/store.rs
@@ -12,6 +12,7 @@ pub struct TicketStore {
 }
 
 impl TicketStore {
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self {
             tickets: BTreeMap::new(),
@@ -28,13 +29,16 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        // * in this case we have to wrap the ticket in bot Mutex and Arc
+        self.tickets.insert(id, Arc::new(Mutex::new(ticket)));
         id
     }
 
     // The `get` method should return a handle to the ticket
     // which allows the caller to either read or modify the ticket.
-    pub fn get(&self, id: TicketId) -> Option<todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Option<Arc<Mutex<Ticket>>> {
+        // * Here, the return type is already an Option wrapping and Arc and a Mutex!
+        // * the cloned() method is used to return a clone of the Arc<Mutex<Ticket>> value stored in the HashMap. Let's break it down:
+        self.tickets.get(&id).cloned()
     }
 }
